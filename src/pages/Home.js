528dd80c0deda7f99508b135dc6bb530184dd94b
AdnanTurjoy <aturjoy4@gmail.com>
2022-11-09 14:06:41 +0600

    make it better

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function Home(props) {
   return (
     <div className="bg-grey-lighter min-h-screen flex flex-col">
+      <h1>Project Name: "Ami Coding Pari Na"</h1>
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
         <div className="bg-white px-6 py-8 rounded shadow-md text-black w-">
           <Link to={"/khoj"}>
